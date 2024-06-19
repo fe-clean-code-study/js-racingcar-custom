6e9225ed7da6cf4f129014d8270d6d7c5f09cba7
seeyoujeong <chan3358@naver.com>
2024-06-19 15:35:46 +0900

    feat: getRandomNumber 함수 추가

---
+++ src/utils/getRandomNumber.js
@@ -0,0 +1,8 @@
+function getRandomNumber(min, max) {
+  if (min > max)
+    throw new Error("The min number must be greater than the max number.");
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export default getRandomNumber;
---
+++ src/utils/index.js
@@ -0,0 +1,1 @@
+export { default as getRandomNumber } from "./getRandomNumber.js";
