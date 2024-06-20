178104d217845c9b7769c3479e63d97c55c9fea2
seeyoujeong <chan3358@naver.com>
2024-06-20 09:15:01 +0900

    test: getRandomNumber 함수 테스트 코드 추가

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -13,11 +13,19 @@ describe("getRandomNumber 함수 테스트", () => {
     [7, 17],
     [8, 18],
     [9, 19],
-  ])("%i 이상 %i 이하의 정수가 무작위로 생성됩니다.", (min, max) => {
+  ])("%i 이상 %i 이하의 정수가 무작위로 생성된다.", (min, max) => {
     for (let i = 0; i < 100; i++) {
       expect(getRandomNumber(min, max))
         .toBeGreaterThanOrEqual(min)
         .toBeLessThanOrEqual(max);
     }
+  });
+
+  test("첫번째 인수와 두번째 인수가 같으면 오류가 발생하지 않는다.", () => {
+    expect(() => getRandomNumber(3, 3)).not.toThrowError();
+  });
+
+  test("두번째 인수보다 첫번째 인수가 크면 오류가 발생한다.", () => {
+    expect(() => getRandomNumber(4, 2)).toThrowError();
   });
 });
