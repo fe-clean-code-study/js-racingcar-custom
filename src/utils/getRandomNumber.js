9d26bd0524530dfe49c47307f43256ba9b912f98
seeyoujeong <chan3358@naver.com>
2024-06-20 09:27:47 +0900

    style: 중괄호 추가

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,6 +1,7 @@
 function getRandomNumber(min, max) {
-  if (min > max)
+  if (min > max) {
     throw new Error("The min number must be greater than the max number.");
+  }
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
