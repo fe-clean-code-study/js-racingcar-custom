14ee75e6e0174abd3090ce3347662a99e5d92109
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-06-21 11:07:02 +0900

    feat: 반복 함수 구현

---
+++ src/utils/repeatFn.js
@@ -0,0 +1,21 @@
+/**
+ * 콜백 함수를 전달된 횟수만큼 반복 실행
+ *
+ * @param {function} fn - 반복 실행할 콜백 함수
+ * @param {number} times - 반복 횟수
+ *
+ * @example
+ * // 0부터 10까지의 랜덤 숫자 반환
+ * getRandomNumber(10);
+ *
+ * @example
+ * // 5부터 15까지의 랜덤 숫자 반환
+ * getRandomNumber(5, 15);
+ */
+function repeatFn(fn, times = 1) {
+  for (let i = 0; i < times; i++) {
+    fn();
+  }
+}
+
+export default repeatFn;
