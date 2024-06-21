46ddaf0190e9fe5b27bb40d06e3606910ff39f7d
seeyoujeong <chan3358@naver.com>
2024-06-21 09:59:56 +0900

    test: Car 클래스 테스트 코드 설명 수정 및 불필요한 테스트 코드 삭제

--- src/__tests__/Car.test.js
+++ src/__tests__/Car.test.js
@@ -10,13 +10,13 @@ describe("Car 클래스 테스트", () => {
     { value: {} },
     { value: [] },
   ])(
-    "Car 클래스의 인수값으로 문자열이 아닌 값($value)을 할당하면 오류가 발생한다.",
+    "자동차의 이름으로 문자열이 아닌 값($value)을 할당하면 오류가 발생한다.",
     ({ value }) => {
       expect(() => new Car(value)).toThrowError();
     }
   );
 
-  test("Car 클래스의 인수값으로 문자열을 할당하면 오류가 발생하지 않는다.", () => {
+  test("자동차의 이름으로 문자열을 할당하면 오류가 발생하지 않는다.", () => {
     expect(() => new Car("car")).not.toThrowError();
   });
 
@@ -26,12 +26,5 @@ describe("Car 클래스 테스트", () => {
 
   test("자동차 이름이 1자 미만이면 오류가 발생한다.", () => {
     expect(() => new Car("")).toThrowError();
-  });
-
-  test("자동차는 움직일 수 있다.", () => {
-    const car = new Car("test");
-    car.move();
-
-    expect(car.position).toBe(1);
   });
 });
