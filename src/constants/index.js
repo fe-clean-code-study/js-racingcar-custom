fef6112c66cdd26984e116972d86be4bdb8fb5d0
Suyeon <msjgr2801@gmail.com>
2024-06-21 15:44:58 +0900

    feat: 게임 진행 로직 추가

---
+++ src/constants/index.js
@@ -0,0 +1,3 @@
+export const GAME = {
+  ROUND: 5,
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import Racing from './model/Racing.js';
+import { GAME } from './constants/index.js';
+import InputView from './view/InputView.js';
+import OutputView from './view/OutputView.js';
+
+class Main {
+  constructor(rounds) {
+    this.rounds = rounds;
+  }
+
+  async play() {
+    const result = this.rounds.map(async (round) => {
+      const cars = await InputView.getCarName();
+      const racing = new Racing(round, cars);
+      const winners = racing.startRacing(cars);
+
+      OutputView.printWinners(winners);
+      return winners;
+    });
+
+    return result;
+  }
 }
 
-main();
+const main = new Main([GAME.ROUND]);
+main.play();
