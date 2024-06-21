dff34ae7a0227d5e40a77d25a81b79c77d38bd0f
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-06-21 17:43:10 +0900

    refactor: 기존 유효성 검사 로직에 숫자 Validator 적용

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,14 +1,14 @@
-import { ERROR_MESSAGES } from '../constants/errorMessages.js';
+import NumberValidator from '../shared/NumberValidator.js';
 
 /**
  * 주어진 범위 내에서 랜덤 숫자를 반환하는 함수
  *
- * @param {number} num1 - num1만 전달된 경우 0부터 num1 중 랜덤 값을 반환
- * @param {number} [num2] - (선택적) 전달된 경우 num1부터 num2 중 랜덤 값을 반환
+ * @param {number} num1
+ * @param {number} num2
  *
  * @example
  * // 0부터 10까지의 랜덤 숫자 반환
- * getRandomNumber(10);
+ * getRandomNumber(0, 10);
  *
  * @example
  * // 5부터 15까지의 랜덤 숫자 반환
@@ -17,28 +17,10 @@ import { ERROR_MESSAGES } from '../constants/errorMessages.js';
 function getRandomNumber(num1, num2) {
   let random;
 
-  if (
-    typeof num1 !== 'number' ||
-    (typeof num2 !== 'undefined' && typeof num2 !== 'number')
-  ) {
-    throw new Error(ERROR_MESSAGES.INVALID_NUMBER);
-  }
+  NumberValidator.from(num1).lessThanOrEqual(num2);
+  NumberValidator.from(num2);
 
-  if (num1 > num2) {
-    throw new Error(ERROR_MESSAGES.INVALID_RANGE);
-  }
-
-  if (typeof num2 !== 'undefined') {
-    random = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
-    return parseInt(random);
-  }
-
-  if (num1 < 0) {
-    random = Math.ceil(Math.random() * (num1 - 1));
-    return parseInt(random);
-  }
-
-  random = Math.floor(Math.random() * (num1 + 1));
+  random = Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
   return parseInt(random);
 }
 
