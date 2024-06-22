b201eac90f41ab3f73984a79cb98cf3b5934df01
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-06-22 18:26:33 +0900

    refactor: Vehicle에 상속받도록 변경

--- src/domain/Car.js
+++ src/domain/Car.js
@@ -1,15 +1,16 @@
-import { NumberValidator, StringValidator } from '../utils/validators';
+import Vehicle from '../shared/Vehicle.js';
+import NumberValidator from '../utils/validators/NumberValidator.js';
+import StringValidator from '../utils/validators/StringValidator.js';
 
-class Car {
+class Car extends Vehicle {
   static #NAME_SIZE_LIMIT = 5;
   static #NAME_PATTERN = /^[a-zA-Z0-9ㄱ-ㅎ가-힣\s]*$/;
 
   #name;
-  #position;
 
   constructor({ name, nameSize = Car.#NAME_SIZE_LIMIT }) {
+    super();
     Car.#validateName(name.trim(), nameSize);
-    this.#position = 0;
     this.#name = name.trim();
     this.mode;
   }
@@ -18,17 +19,9 @@ class Car {
     return this.#name;
   }
 
-  get position() {
-    return this.#position;
-  }
-
-  move() {
-    this.#position += 1;
-  }
-
   static #validateName(name, nameSize) {
     // 이름 유효성 검사
-    StringValidator.from(name).matches(this.#NAME_PATTERN);
+    StringValidator.from(name).notEmpty().matches(this.#NAME_PATTERN);
 
     // 이름 길이 유효성 검사
     NumberValidator.from(nameSize);
