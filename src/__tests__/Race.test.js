12de23c399a8c4740baac80bcc1eb7b5316496bd
seeyoujeong <chan3358@naver.com>
2024-06-22 22:13:28 +0900

    test: Race 클래스의 ready 메서드 관련 테스트 코드 추가

--- src/__tests__/Race.test.js
+++ src/__tests__/Race.test.js
@@ -36,4 +36,46 @@ describe("Race 클래스 테스트", () => {
   test("레이스 횟수가 1이상이면 오류가 발생하지 않는다.", () => {
     expect(() => new Race(Car, 1)).not.toThrowError();
   });
+
+  test.each([
+    { value: 1 },
+    { value: true },
+    { value: undefined },
+    { value: null },
+    { value: {} },
+    { value: [] },
+  ])(
+    "레이스 준비시 문자열이 아닌 값($value)을 할당하면 오류가 발생한다.",
+    ({ value }) => {
+      const race = raceWithCarAndLaps();
+
+      expect(() => race.ready(value)).toThrowError(
+        "경기 준비에 적합하지 않은 입력값입니다."
+      );
+    }
+  );
+
+  test("레이스 준비시 문자열을 할당하면 오류가 발생하지 않는다.", () => {
+    const race = raceWithCarAndLaps();
+
+    expect(() => race.ready("123")).not.toThrowError();
+  });
+
+  test("레이스 준비의 입력값으로 0자 이하면 오류가 발생한다.", () => {
+    const race = raceWithCarAndLaps();
+
+    expect(() => race.ready("")).toThrowError(
+      "경기 준비의 입력값은 1자 이상이어야 합니다."
+    );
+  });
+
+  test("레이스 준비의 입력값으로 1자 이상이면 오류가 발생하지 않는다.", () => {
+    const race = raceWithCarAndLaps();
+
+    expect(() => race.ready("123")).not.toThrowError();
+  });
 });
+
+function raceWithCarAndLaps() {
+  return new Race(Car, 1);
+}
