6d7892bfa606307668f800bcb20baba753f39b58
jgjgill <dbdltm22@naver.com>
2024-06-22 21:26:26 +0900

    feat: 자동차 함수 코드 수정 상수 분리 및 메서드 반환값 받는 형태로 변경

--- src/domain/car.js
+++ src/domain/car.js
@@ -1,23 +1,32 @@
-export function Car(name, now = 0) {
+const VALID_NAME_LENGTH = 5;
+const DEFAULT_CAR_NOW = 0;
+
+const ERROR_NAME_LABEL = "이름은 5자 이하만 가능합니다.";
+
+/**
+ * @type {import("./race").Target}
+ */
+
+export function Car(name, now = DEFAULT_CAR_NOW) {
   if (!isVaildName(name)) {
-    throw new Error("이름은 5자 이하만 가능합니다.");
+    throw new Error(ERROR_NAME_LABEL);
   }
 
   function setMovement(distance) {
     now += distance;
   }
 
-  function tellName() {
-    console.log(name);
+  function getNow() {
+    return now;
   }
 
-  function tellNow() {
-    console.log(now);
+  function getName() {
+    return name;
   }
 
-  return { tellName, tellNow, setMovement };
+  return { setMovement, getNow, getName };
 }
 
 function isVaildName(name) {
-  return name.length <= 5;
+  return name.length <= VALID_NAME_LENGTH;
 }
