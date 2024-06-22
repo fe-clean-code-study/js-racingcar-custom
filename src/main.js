864e5defda8e914bd539104e7505217c86b0dd4c
jgjgill <dbdltm22@naver.com>
2024-06-22 21:26:49 +0900

    faet: 잘못된 입력 받으면 에러 발생 및 재시도

--- src/main.js
+++ src/main.js
@@ -1,11 +1,37 @@
 import readline from "readline";
 import { Car } from "./domain/car.js";
+import { Race } from "./domain/race.js";
 
-function main() {
-  play();
+const INPUT_NAME_LABEL =
+  "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분). ";
+const INPUT_COUNT_LABEL = "시도할 회수는 몇회인가요? ";
+const ALLOWED_MOVE_NUMBER = 4;
+
+play();
+
+async function play() {
+  try {
+    const names = await readLineAsync(INPUT_NAME_LABEL);
+    const count = await readLineAsync(INPUT_COUNT_LABEL);
+
+    const race = Race({
+      names,
+      Target: Car,
+      isValidMove,
+      count: Number(count),
+    });
+
+    race.start();
+    race.end();
+  } catch (err) {
+    console.log(err.message);
+    play();
+  }
 }
 
-main();
+function isValidMove() {
+  return Math.random() * 10 >= ALLOWED_MOVE_NUMBER;
+}
 
 function readLineAsync(query) {
   return new Promise((resolve, reject) => {
@@ -27,11 +53,4 @@ function readLineAsync(query) {
       resolve(input);
     });
   });
-}
-
-async function play() {
-  const name = await readLineAsync("자동차 이름을 입력하세요 > ");
-  const car = Car(name);
-  car.tellName();
-  car.tellNow();
 }
