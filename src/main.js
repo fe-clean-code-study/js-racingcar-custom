c561c3f5920e4fa4f0187db665559b9d2ae426f1
jkea1 <jinkyng7890@gmail.com>
2024-06-24 01:23:12 +0900

    feat: 자동차 이름 입력 값 로그로 확인

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import { createCar } from './domains/Car.js'
+import { inputCarNames } from './views/inputCarNames.js'
+
+const RACE_ROUND = 5
+
+async function main() {
+  const names = await inputCarNames()
+
+  const cars = names.map((name) => createCar(name))
+
+  console.log('결과 확인 >', cars)
 }
 
-main();
+main()
