62a8b213511ae2e4ec630d02da19ff9d51bc18ef
jkea1 <jinkyng7890@gmail.com>
2024-06-24 17:09:02 +0900

    feat: 경기 진행 내용

--- src/main.js
+++ src/main.js
@@ -1,14 +1,33 @@
-import { createCar } from './domains/Car.js'
-import { inputCarNames } from './views/inputCarNames.js'
+import { createCar, moveCar } from './domains/car.js'
+import { getWinners } from './domains/wInner.js'
+import { inputCarNames } from './views/inputs.js'
+import { printCar, printWinners } from './views/outputs.js'
 
 const RACE_ROUND = 5
 
 async function main() {
   const names = await inputCarNames()
 
-  const cars = names.map((name) => createCar(name))
+  let cars = names.map((name) => createCar(name))
 
-  console.log('결과 확인 >', cars)
+  console.log('✨ 실행 결과 ✨')
+  console.log()
+
+  for (let i = 0; i < RACE_ROUND; i++) {
+    cars = cars.map((car) => {
+      let movedCar = moveCar(car)
+
+      printCar(movedCar)
+
+      return movedCar
+    })
+
+    console.log() // 여백 역할
+  }
+
+  const winners = getWinners(cars)
+
+  printWinners(winners)
 }
 
 main()
