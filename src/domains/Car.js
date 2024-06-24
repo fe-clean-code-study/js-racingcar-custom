f5d48cbb80bbef67e13922edac758c9c385e9eac
jkea1 <jinkyng7890@gmail.com>
2024-06-24 17:12:37 +0900

    feat: 차 관련 함수 생성

--- src/domains/Car.js
+++ src/domains/Car.js
@@ -1,6 +1,6 @@
+import { getRandomNum } from '../utils/getRandomNum.js'
 import { validateCarName } from '../validations/validateCarName.js'
-
-const NAME_SIZE = 5
+import { validateRandomNum } from '../validations/validateRandomNum.js'
 
 export const createCar = (name) => {
   validateCarName(name)
@@ -11,9 +11,14 @@ export const createCar = (name) => {
   }
 }
 
-export const moveCar = (car) => ({
-  ...car,
-  position: car.position + 1,
-})
+export const moveCar = (car) => {
+  let randomNum = getRandomNum()
 
-export const getPosition = (car) => car.position
+  if (validateRandomNum(randomNum)) {
+    return { ...car, position: car.position + 1 }
+  } else {
+    return car
+  }
+}
+
+// export const getPosition = (car) => car.position
