a27071eeafb1ce4400d11ddf39ca201610f2d00f
jkea1 <jinkyng7890@gmail.com>
2024-06-24 17:05:51 +0900

    feat: outputs 관련 함수 생성

---
+++ src/views/outputs.js
@@ -0,0 +1,7 @@
+export const printCar = (car) => {
+  console.log(`${car.name}: ${'-'.repeat(car.position)}`)
+}
+
+export const printWinners = (winners) => {
+  console.log(`🎉 ${winners.join(', ')}가 최종 우승했습니다.`)
+}
