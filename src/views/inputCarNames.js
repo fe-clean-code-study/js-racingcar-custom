b30b878cb38d8049e2652e7df956ddb9945a7cb8
jkea1 <jinkyng7890@gmail.com>
2024-06-24 17:07:17 +0900

    chore: 폴더 구조 변경

--- src/views/inputCarNames.js
+++
@@ -1,9 +0,0 @@
-import readLineAsync from './readLineAsync.js'
-
-export const inputCarNames = async () => {
-  const names = await readLineAsync(
-    '경주할 자동차 이름을 입력하세요! (이름은 쉼표(,)를 기준으로 구분해 주세요!) > '
-  )
-
-  return names.split(',').map((name) => name.trim())
-}
