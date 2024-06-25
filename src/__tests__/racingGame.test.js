18c3677a60d6777dc9cfd020e5e262b4c45fe906
chacha <chasj0326@naver.com>
2024-06-25 13:08:31 +0900

    refactor: 테스트 코드 주석 설명 추가

--- src/__tests__/racingGame.test.js
+++ src/__tests__/racingGame.test.js
@@ -18,7 +18,7 @@ describe('RacingGame', () => {
     display = {
       print: vi.fn(),
       printError: vi.fn(),
-      read: vi.fn().mockResolvedValue('car1,car2,car3'),
+      read: vi.fn(),
     }
 
     racingGame = new RacingGame({
@@ -28,6 +28,7 @@ describe('RacingGame', () => {
     })
   })
 
+  // 출력된 문자열을 확인하기 위해 display 호출의 매개변수를 가져오는 함수
   const getDisplayText = type => {
     return display[type === 'error' ? 'printError' : 'print'].mock.calls.map(
       call => call[0]
@@ -35,17 +36,23 @@ describe('RacingGame', () => {
   }
 
   test('게임에 참여하는 자동차 이름은 각각 달라야 한다.', async () => {
+    // display.read 에서 겹치는 자동차 입력값을 읽어온 것으로 모킹
     display.read.mockResolvedValue('car1,car2,car1')
+
+    // matcher 가 1번은 실행되야 함 (toBe 메소드가 실행되어야 함 = 에러 발생해야 함)
     expect.assertions(1)
     try {
       await racingGame.setup()
     } catch (error) {
+      // 에러 메시지 확인
       expect(error.message).toBe(racingValidations.uniqueCarName.errorMessage)
     }
   })
 
   test('게임에는 최소 2대의 자동차가 참여해야 한다.', async () => {
+    // display.read 에서 1대 자동차를 읽어온 것으로 모킹
     display.read.mockResolvedValue('car1')
+
     expect.assertions(1)
     try {
       await racingGame.setup()
@@ -58,24 +65,29 @@ describe('RacingGame', () => {
     racingGame.carNames = ['car1']
     racingGame.cars = { car1: new Car('car1') }
 
+    // getRandomNumber 에서 설정한 가짜 값을 반환하도록 spyOn 호출
     const mockGetRandomNumber = vi.spyOn(randomNumberModule, 'getRandomNumber')
+
+    // 기준치 초과, 또는 기준치 이하를 반환하도록 하여 테스트
     mockGetRandomNumber.mockReturnValue(
       move ? testRacingConfig.threshold + 1 : testRacingConfig.threshold
     )
-
+    // move 함수 모킹
     racingGame.cars['car1'].move = vi.fn()
     racingGame.eachRound()
 
-    if (move) expect(racingGame.cars['car1'].move).toBeCalled()
-    else expect(racingGame.cars['car1'].move).not.toBeCalled()
+    if (move) expect(racingGame.cars['car1'].move).toBeCalled() // '전진한다' 테스트
+    else expect(racingGame.cars['car1'].move).not.toBeCalled()  // '움직이지 않는다' 테스트
     mockGetRandomNumber.mockRestore()
   }
 
   test('자동차는 라운드에서 랜덤 숫자가 설정값보다 크면 전진한다.', () => {
+    // getRandomNumber 가 기준치 초과해서 반환했을 때의 테스트 작동
     testCarMovement(true)
   })
 
   test('자동차는 라운드에서 랜덤 숫자가 설정값보다 작거나 같으면 움직이지 않는다', () => {
+    // getRandomNumber 가 기준치 이하을 반환했을 때의 테스트 작동
     testCarMovement(false)
   })
 
@@ -86,12 +98,18 @@ describe('RacingGame', () => {
       car2: new Car('car2'),
       car3: new Car('car3'),
     }
+
+    // 1,3 번 자동차만 이동
     racingGame.cars['car1'].move()
     racingGame.cars['car3'].move()
+
+    // winner 확인
     expect(racingGame.winners).toEqual(['car1', 'car3'])
 
     racingGame.finish()
     const winnerMessage = getDisplayText()
+
+    // 우승자 출력 확인
     expect(winnerMessage).toContain('car1')
     expect(winnerMessage).toContain('car3')
   })
