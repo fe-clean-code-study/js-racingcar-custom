07e46217ae6961d8af309611c3e83991ae2babb2
seeyoujeong <chan3358@naver.com>
2024-06-25 21:45:28 +0900

    refactor: 유효성 검사 메서드 추가

--- src/domain/RacerRegistry.js
+++ src/domain/RacerRegistry.js
@@ -1,23 +1,45 @@
 import { inputManager } from "../service/index.js";
 
 class RacerRegistry {
-  #label;
+  #entityType;
   #separator;
 
-  constructor(label, separator) {
-    this.#label = label;
+  constructor(entityType, separator) {
+    RacerRegistry.#validateEntityType(entityType);
+    RacerRegistry.#validateSeparator(separator);
+    this.#entityType = entityType;
     this.#separator = separator;
   }
 
   async register() {
     const inputValue = await inputManager.scan(
-      `경주할 ${this.#label} 이름을 입력하세요(이름은 ${
+      `경주할 ${this.#entityType} 이름을 입력하세요(이름은 ${
         this.#separator
       }를 기준으로 구분).\n`
     );
     const racerNameList = inputValue.split(this.#separator);
 
     return racerNameList;
+  }
+
+  static #validateEntityType(entityType) {
+    if (typeof entityType !== "string") {
+      throw new Error("개체 유형은 문자열이어야 합니다.");
+    }
+
+    if (entityType.length < 1) {
+      throw new Error("개체 유형은 1자 이상이어야 합니다.");
+    }
+  }
+
+  static #validateSeparator(separator) {
+    if (typeof separator !== "string") {
+      throw new Error("분리 문자는 문자열이어야 합니다.");
+    }
+
+    if (separator.length < 1) {
+      throw new Error("분리 문자는 1자 이상이어야 합니다.");
+    }
   }
 }
 
