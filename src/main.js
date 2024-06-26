4b684d4077873853833b902ef4f640bc0328a279
hysngg <hs.kim@teamsparta.co>
2024-06-26 21:45:05 +0900

    feat : main.js 수정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-function main() {
-  console.log('main의 내용을 채워주세요');
+import { RaceView } from "./view/RaceView.js";
+
+async function main() {
+  const raceView = new RaceView();
+  await raceView.enrollCars();
+  await raceView.startRace();
+  raceView.controller.endRace();
 }
 
 main();
