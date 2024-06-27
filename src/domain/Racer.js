d258028e9d92bb817c9bacecc1685ba3a587f294
seeyoujeong <chan3358@naver.com>
2024-06-27 14:58:14 +0900

    feat: move 메서드에 distance 매개변수 추가

--- src/domain/Racer.js
+++ src/domain/Racer.js
@@ -16,8 +16,18 @@ class Racer {
     return this.#position;
   }
 
-  move() {
-    this.#position++;
+  move(distance = 1) {
+    Racer.#validateDistance(distance);
+
+    const movedPosition = this.#position + distance;
+
+    this.#position = movedPosition < 0 ? 0 : movedPosition;
+  }
+
+  static #validateDistance(distance) {
+    if (typeof distance !== "number" || !Number.isInteger(distance)) {
+      throw new Error("이동 거리는 정수여야 합니다.");
+    }
   }
 }
 
