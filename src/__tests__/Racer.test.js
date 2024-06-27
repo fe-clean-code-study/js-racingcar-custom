4bf618eec256b22b06ecd5a2ace426cff05911d6
seeyoujeong <chan3358@naver.com>
2024-06-27 14:32:46 +0900

    test: 기존 테스트 가독성 개선

--- src/__tests__/Racer.test.js
+++ src/__tests__/Racer.test.js
@@ -1,22 +1,22 @@
-import { beforeEach, describe, expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { Racer } from "../domain/index.js";
 
 describe("Racer 클래스 테스트", () => {
-  let racer;
-
-  beforeEach(() => {
-    racer = new Racer(" jeong ");
-  });
-
   test("레이서의 이름 앞뒤 공백은 제거된다.", () => {
+    const racer = new Racer(" jeong ");
+
     expect(racer.name).toHaveLength(5);
   });
 
   test("레이서에 이름을 지어줄 수 있다.", () => {
-    expect(racer.name).toBe("jeong");
+    const racer = new Racer("tom");
+
+    expect(racer.name).toBe("tom");
   });
 
   test("레이서는 움직일 수 있다.", () => {
+    const racer = createRacerWithName();
+
     racer.move();
 
     expect(racer.position).toBe(1);
