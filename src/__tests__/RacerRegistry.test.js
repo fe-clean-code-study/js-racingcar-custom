37e3e4e34444137d7620ebb974e707cfc93c5f6c
seeyoujeong <chan3358@naver.com>
2024-06-28 17:41:25 +0900

    test: 유형 선택 관련 테스트 코드 추가

--- src/__tests__/RacerRegistry.test.js
+++ src/__tests__/RacerRegistry.test.js
@@ -1,4 +1,19 @@
 import { describe, expect, test, vi } from "vitest";
 import { RacerRegistry } from "../domain/index.js";
+import { inputManager } from "../service/index.js";
 
-describe("RacerRegistry 클래스 테스트", () => {});
+vi.mock("../service/index.js");
+
+describe("RacerRegistry 클래스 테스트", () => {
+  test("올바른 유형을 선택하지 않으면 오류가 발생한다.", async () => {
+    inputManager.retryScan.mockImplementationOnce(async (_, { processFn }) => {
+      return processFn("5");
+    });
+
+    const register = new RacerRegistry();
+
+    await expect(register.selectEntityType()).rejects.toThrowError(
+      "올바른 유형의 번호가 아닙니다."
+    );
+  });
+});
