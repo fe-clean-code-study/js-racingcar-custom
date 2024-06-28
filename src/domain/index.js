04fde6da1d100877605169fd16a831e668edf4e7
seeyoujeong <chan3358@naver.com>
2024-06-28 16:28:20 +0900

    fix: 삭제한 Car 클래스 export하는 코드 삭제

--- src/domain/index.js
+++ src/domain/index.js
@@ -1,4 +1,3 @@
-export { default as Car } from "./Car.js";
 export { default as Race } from "./Race.js";
 export { default as Racer } from "./Racer.js";
 export { default as RaceScoreboard } from "./RaceScoreboard.js";
