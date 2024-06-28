ab76a7947506b17b0e6ae689eaf2b8fa5c66399d
Suyeon <msjgr2801@gmail.com>
2024-06-28 17:01:01 +0900

    refactoring: Car 에서 Racer 에 해당하는 역할 분리하기

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,61 +1,8 @@
-class Car {
-  constructor(name) {
-    name = name.trim();
+import Racer from './Racer.js';
 
-    this._name = name;
-    this._position = 0;
-  }
-
-  /**
-   * @param {string} name
-   */
-  set name(name) {
-    Car.validateName(name);
-    this._name = name;
-  }
-
-  /**
-   * @param {number} position
-   */
-  set position(position) {
-    Car.validatePosition(position);
-    this._position = position;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get position() {
-    return this._position;
-  }
-
-  move(distance = 1, rules) {
-    if (rules && Array.isArray(rules)) {
-      if (rules.some((rule) => rule() === false)) {
-        return false;
-      }
-    }
-
-    this.position = this.position + distance;
-
-    return true;
-  }
-
-  static validateName(name) {
-    if (typeof name !== 'string') {
-      throw new Error('자동자 이름은 문자열이어야 합니다.');
-    }
-
-    if (name.length < 1 || name.length > 5) {
-      throw new Error('자동차 이름은 1 ~ 5 글자로 이뤄져야 합니다.');
-    }
-  }
-
-  static validatePosition(position) {
-    if (typeof position !== 'number') {
-      throw new Error('자동차의 위치는 숫자 형태여야 합니다.');
-    }
+class Car extends Racer {
+  constructor({ name, position = 0 }) {
+    super({ name, position });
   }
 }
 
---
+++ src/model/Racer.js
@@ -0,0 +1,69 @@
+export default class Racer {
+  _name;
+  _position;
+
+  constructor({ name, position }) {
+    name = name.trim();
+
+    this.name = name;
+    this.position = position;
+  }
+
+  set name(name) {
+    const error = Racer.getNameError(name);
+
+    if (error !== undefined) {
+      throw new Error(error);
+    }
+
+    this._name = name;
+  }
+
+  set position(position) {
+    const error = Racer.getPositionError(position);
+
+    if (error !== undefined) {
+      throw new Error(error);
+    }
+
+    this._position = position;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get position() {
+    return this._position;
+  }
+
+  init() {
+    this._position = 0;
+  }
+
+  move(distance = 1) {
+    this.position = this.position + distance;
+
+    return true;
+  }
+
+  static getNameError(name) {
+    if (typeof name !== 'string') {
+      return '레이서들의 이름은 문자열이어야 합니다.';
+    }
+
+    if (name.length < 1 || name.length > 5) {
+      return '레이서들의 이름은 1 ~ 5 글자로 이뤄져야 합니다.';
+    }
+
+    return undefined;
+  }
+
+  static getPositionError(position) {
+    if (typeof position !== 'number') {
+      return '레이서들의 위치는 숫자 형태여야 합니다.';
+    }
+
+    return undefined;
+  }
+}
