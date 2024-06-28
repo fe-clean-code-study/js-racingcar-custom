cb0bb7a9e3fe3dea57e8eb140b6f5a2f8d0c97ee
seeyoujeong <chan3358@naver.com>
2024-06-28 15:16:16 +0900

    refactor: retryScan 메서드 파라미터 수정

--- src/service/inputManager.js
+++ src/service/inputManager.js
@@ -13,19 +13,16 @@ class InputManager {
     return inputValue.trim();
   }
 
-  async retryScan(query, validateFn, errorMessageQuery = "") {
+  async retryScan(query, { processFn, errorMessageQuery = "" }) {
     try {
       const inputValue = await this.scan(query);
 
-      validateFn(inputValue);
-
-      return inputValue;
+      return processFn ? processFn(inputValue) : inputValue;
     } catch (error) {
-      return await this.retryScan(
-        `${error.message} ${errorMessageQuery}`,
-        validateFn,
-        errorMessageQuery
-      );
+      return await this.retryScan(`${error.message} ${errorMessageQuery}`, {
+        processFn,
+        errorMessageQuery,
+      });
     }
   }
 }
