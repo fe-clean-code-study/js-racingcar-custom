687fc97136b3cb7457798ad219c9c432f2b544a4
Suyeon <msjgr2801@gmail.com>
2024-06-28 16:59:18 +0900

    refactoring: 컨트롤러의 역할을 main 으로 변경

--- src/main.js
+++ src/main.js
@@ -1,16 +1,55 @@
-import GameController from './controller/GameController.js';
+import { GAME } from './constants/game.js';
+import Race from './model/Race.js';
+import Car from './model/Car.js';
+import GameView from './view/RaceView.js';
+import gameSupport from './utils/gameSupport.js';
 
-class Main {
-  constructor() {
-    this.gameController = new GameController();
+async function main() {
+  const view = new GameView();
+  let roundInput = undefined;
+  let namesInput = undefined;
+
+  while (true) {
+    roundInput = await view.inputRound();
+    const error = Race.getRoundError(roundInput);
+
+    if (error === undefined) {
+      break;
+    }
+    view.printError(error);
   }
 
-  play() {
-    this.gameController.start();
+  while (true) {
+    namesInput = await view.inputRacerNames();
+    const error = Race.getNamesError(namesInput);
+
+    if (error === undefined) {
+      break;
+    }
+    view.printError(error);
   }
+
+  const race = new Race({
+    Racer: Car,
+    round: Number(roundInput),
+    names: namesInput,
+    rules: {
+      diceRule: () => {
+        return (
+          gameSupport.dice(GAME.RULE.MAX_DICE, GAME.RULE.MIN_DICE) >=
+          GAME.RULE.DICE_CONDITION
+        );
+      },
+    },
+    onProceed: () => {
+      view.printMovedCar(race.getRacerStatus());
+    },
+  });
+
+  const winners = race.startRace();
+  view.printWinners(winners);
 }
 
-const main = new Main();
-main.play();
+main();
 
 export default main;
