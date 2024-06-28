1ebcfbb8ce384a05a8b04965c7dd248f1a43f380
Suyeon <msjgr2801@gmail.com>
2024-06-28 17:00:20 +0900

    refactoring: racing과 관련된 상수 추가

---
+++ src/constants/game.js
@@ -0,0 +1,11 @@
+export const GAME = {
+  RULE: {
+    MAX_ROUND: 10,
+    MIN_ROUND: 1,
+    MAX_NAME_LENGTH: 10,
+    MIN_NAME_LENGTH: 1,
+    MAX_DICE: 10,
+    MIN_DICE: 0,
+    DICE_CONDITION: 4,
+  },
+};
