f36a9946daf034177c0e2f841ef0d62d82efc729
Suyeon <msjgr2801@gmail.com>
2024-06-28 17:01:44 +0900

    refactoring: 유틸 함수를 default 값으로 묶기

--- src/utils/gameSupport.js
+++ src/utils/gameSupport.js
@@ -1,3 +1,7 @@
-export function dice(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+const gameSupport = {
+  dice(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  },
+};
+
+export default gameSupport;
