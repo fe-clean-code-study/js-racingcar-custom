ab0775d220ba04a2edfaeccc71efc915818b6cc5
seeyoujeong <chan3358@naver.com>
2024-06-28 10:09:11 +0900

    test: 분리 문자 관련 테스트 코드 삭제

--- src/__tests__/RacerRegistry.test.js
+++ src/__tests__/RacerRegistry.test.js
@@ -1,30 +1,4 @@
 import { describe, expect, test, vi } from "vitest";
 import { RacerRegistry } from "../domain/index.js";
 
-describe("RacerRegistry 클래스 테스트", () => {
-  test.each([
-    { value: 1 },
-    { value: true },
-    { value: undefined },
-    { value: null },
-    { value: {} },
-    { value: [] },
-  ])(
-    "분리 문자로 문자열이 아닌($value)을 할당하면 오류가 발생한다.",
-    ({ value }) => {
-      expect(() => new RacerRegistry(value)).toThrowError();
-    }
-  );
-
-  test("분리 문자로 문자열을 할당하면 오류가 발생하지 않는다.", () => {
-    expect(() => new RacerRegistry(",")).not.toThrowError();
-  });
-
-  test("분리 문자가 1자 미만이면 오류가 발생한다.", () => {
-    expect(() => new RacerRegistry("")).toThrowError();
-  });
-
-  test("분리 문자가 1자 이상이면 오류가 발생하지 않는다.", () => {
-    expect(() => new RacerRegistry("-")).not.toThrowError();
-  });
-});
+describe("RacerRegistry 클래스 테스트", () => {});
