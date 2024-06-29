aa068696c93f55588c76542ee9a50f2ec295d819
seeyoujeong <chan3358@naver.com>
2024-06-29 16:45:27 +0900

    feat: 레이스의 횟수 입력 받는 기능 임시 구현

--- src/main.js
+++ src/main.js
@@ -1,11 +1,29 @@
 import { Race, RaceScoreboard, RacerRegistry } from "./domain/index.js";
+import inputManager from "./service/inputManager.js";
 
 async function main() {
   const racerRegistry = new RacerRegistry();
   await racerRegistry.selectEntityType();
   const racers = await racerRegistry.register();
 
-  const race = new Race(5);
+  const laps = await inputManager.retryScan("시도할 횟수는 몇회인가요?\n", {
+    processFn: (inputValue) => {
+      const numValue = Number(inputValue);
+
+      if (Number.isNaN(numValue) || !Number.isInteger(numValue)) {
+        throw new Error("시도할 횟수로 정수를 입력해야 합니다.");
+      }
+
+      if (numValue < 1) {
+        throw new Error("시도할 횟수는 1이상이어야 합니다.");
+      }
+
+      return numValue;
+    },
+    errorMessageQuery: "다시 입력해주세요.\n",
+  });
+
+  const race = new Race(laps);
   race.start(racers);
 
   const raceScoreboard = new RaceScoreboard(race);
