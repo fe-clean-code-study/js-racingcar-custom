5820cacbe0d01dd27febee2fc5cbfda0084024d8
seeyoujeong <chan3358@naver.com>
2024-06-30 20:39:42 +0900

    feat: Validator 클래스 추가

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
 export { default as getRandomNumber } from "./getRandomNumber.js";
 export { default as deepCopy } from "./deepCopy.js";
 export { default as readLineAsync } from "./readLineAsync.js";
+export { default as validate } from "./validate.js";
---
+++ src/utils/validate.js
@@ -0,0 +1,39 @@
+class Validator {
+  static throwErrorWithCondition(condition, errorMessage) {
+    if (condition) {
+      throw new Error(errorMessage);
+    }
+  }
+
+  static string(value, errorMessage) {
+    Validator.throwErrorWithCondition(typeof value !== "string", errorMessage);
+  }
+
+  static integer(value, errorMessage) {
+    Validator.throwErrorWithCondition(
+      typeof value !== "number" || !Number.isInteger(value),
+      errorMessage
+    );
+  }
+
+  static array(value, errorMessage) {
+    Validator.throwErrorWithCondition(!Array.isArray(value), errorMessage);
+  }
+
+  static lessThan(value, otherValue, errorMessage) {
+    Validator.throwErrorWithCondition(value < otherValue, errorMessage);
+  }
+
+  static instance(value, classValue, errorMessage) {
+    Validator.throwErrorWithCondition(
+      !(value instanceof classValue),
+      errorMessage
+    );
+  }
+
+  static property(value, objectValue, errorMessage) {
+    Validator.throwErrorWithCondition(!(value in objectValue), errorMessage);
+  }
+}
+
+export default Validator;
