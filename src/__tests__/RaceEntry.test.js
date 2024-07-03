b127b7dd5f5cd70d27665f103a0baf40d6a0bd0c
seeyoujeong <chan3358@naver.com>
2024-07-03 15:58:50 +0900

    test: 유형 선택과 레이스 횟수 설정 관련 테스트 추가

--- src/__tests__/RaceEntry.test.js
+++ src/__tests__/RaceEntry.test.js
@@ -1,19 +1,68 @@
 import { describe, expect, test, vi } from "vitest";
-import { RaceEntry } from "../domain/index.js";
+import { Race, RaceEntry } from "../domain/index.js";
 import { inputManager } from "../service/index.js";
 
 vi.mock("../service/index.js");
 
+function retryScanMock(inputValue) {
+  inputManager.retryScan.mockImplementationOnce(async (_, processFn) => {
+    return processFn(inputValue);
+  });
+}
+
 describe("RaceEntry 클래스 테스트", () => {
   test("올바른 유형을 선택하지 않으면 오류가 발생한다.", async () => {
-    inputManager.retryScan.mockImplementationOnce(async (_, processFn) => {
-      return processFn("5");
-    });
+    retryScanMock("5");
 
     const register = new RaceEntry();
 
     await expect(register.selectEntityType()).rejects.toThrowError(
       "올바른 유형의 번호가 아닙니다."
     );
+  });
+
+  test("올바른 유형을 선택하면 오류가 발생하지 않는다.", async () => {
+    retryScanMock("1");
+
+    const register = new RaceEntry();
+
+    await expect(register.selectEntityType()).resolves.not.toThrowError();
+  });
+
+  test("레이스 횟수로 정수가 아닌 값을 입력하면 오류가 발생한다.", async () => {
+    retryScanMock("a");
+
+    const register = new RaceEntry();
+
+    await expect(register.setRaceLaps()).rejects.toThrowError(
+      "시도할 횟수로 정수를 입력해야 합니다."
+    );
+  });
+
+  test("레이스 횟수로 0이하를 입력하면 오류가 발생한다.", async () => {
+    retryScanMock("0");
+
+    const register = new RaceEntry();
+
+    await expect(register.setRaceLaps()).rejects.toThrowError(
+      "시도할 횟수는 1이상이어야 합니다."
+    );
+  });
+
+  test("레이스 횟수로 1이상을 입력하면 오류가 발생하지 않는다.", async () => {
+    retryScanMock("1");
+
+    const register = new RaceEntry();
+
+    await expect(register.setRaceLaps()).resolves.not.toThrowError();
+  });
+
+  test("레이스 횟수를 설정하면 Race 클래스 인스턴스를 반환받는다.", async () => {
+    retryScanMock("1");
+    const register = new RaceEntry();
+
+    const race = await register.setRaceLaps();
+
+    expect(race).toEqual(new Race(1));
   });
 });
